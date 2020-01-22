060b5648c34c10b07608bf4065fb406fa67267d2
Sami Nurmivaara <sami@nurmivaara.fi>
2020-01-22 12:39:56 +0200

    More work on tg integration and added sls offline

--- handler.js
+++ handler.js
@@ -1,7 +1,38 @@
 "use strict";
 
+const axios = require("axios");
+
 module.exports.hello = async event => {
   const token = process.env.TELEGRAM_TOKEN;
+  const base_url = process.env.BASE_URL;
+
+  try {
+    data = event.body;
+    message = JSON.stringify(data.message.text);
+    chat_id = data.message.chat.id;
+    first_name = data.message.chat.first_name;
+
+    response = `TKO-äly bot at your service, type in /start`;
+
+    if (message.contains(start)) response = `Hello ${first_name}`;
+
+    data = {
+      text: response,
+      chat_id: chat_id
+    };
+    url = base_url + `/sendMessage`;
+
+    axios.post(url, data);
+  } catch {
+    return (
+      {
+        statusCode: 500,
+        message: "Unexpected error"
+      },
+      null,
+      2
+    );
+  }
   return {
     statusCode: 200,
     body: JSON.stringify(
