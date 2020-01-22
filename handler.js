64ebfe05c68a46a59e75a18505112ede8e679bab
Sami Nurmivaara <sami@nurmivaara.fi>
2020-01-22 09:39:26 +0200

    Fixed aws stuff and added ssm support

--- handler.js
+++ handler.js
@@ -1,16 +1,20 @@
-'use strict';
+"use strict";
 
-module.exports.hello = async event => {
+const request = require("request");
+
+module.exports = (context, req) => {
+  const token = process.env.TELEGRAM_TOKEN;
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        message: "Go Serverless v1.0! Your function executed successfully!",
+        token: token,
+        input: event
       },
       null,
       2
-    ),
+    )
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
